e5368eb1415dfac487eb1df4676c896ab1c0fd8c
Björn Ali Göransson <bornemix@gmail.com>
2013-08-23 19:06:21 +0200

    Make tests pass

--- transliteration.php
+++ transliteration.php
@@ -35,73 +35,88 @@ function arabic_transliteration_transform($content, $options){
   // move shadda next to letter
   $content = arabic_transliteration_replace("([$standard_harakat$sukun$tanween])($shadda)", "\\2\\1", $content);
 
-
-
-  /* STOP ON SUKUN */
-
   // one-letter words should always have its haraka transliterated
-  $last_word_is_one_letter = preg_match("/(?:^| )[$sun_letters$moon_letters$extraneous_letters][$tashkil]*$/u", $content);
+  if(preg_match("/(?:^| )[$sun_letters$moon_letters$extraneous_letters][$tashkil]*$/u", $content)){
+    $options['stop-on-sukun'] = 0;
+  }
+
+  // anti at end of sentence should always be written out
+  if(preg_match("/(?:^| )[$alef$alef_with_sup_hamza]$fatha?$nun$sukun?$ta$kasra$/u", $content)){
+    $options['stop-on-sukun'] = 0;
+  }
+
+
+
+	/* ALEF WITH WASLA */
   
-	if($options['stop-on-sukun'] && !$last_word_is_one_letter){
-		// tanween
-		$content = arabic_transliteration_replace("$fathatan$alef$", "$fatha$alef", $content);
-		$content = arabic_transliteration_replace("$fathatan$", "", $content);
-		$content = arabic_transliteration_replace("$dammatan$", "", $content);
-		$content = arabic_transliteration_replace("$kasratan$", "", $content);
-		// harakat
-		$content = arabic_transliteration_replace("$fatha$", "", $content);
-		$content = arabic_transliteration_replace("$damma$", "", $content);
-		$content = arabic_transliteration_replace("$kasra$", "", $content);
-	}
-	
+  // unmarked alef with wasla indicated by sukun on next letter
+  $content = arabic_transliteration_replace("(^| )($non_transforming_prefixed_particles)?$alef([$standard_letters_without_lam])$sukun([$standard_letters_without_lam][$fatha$kasra])", "\\1\\2$alef_with_wasla$kasra\\3$sukun\\4", $content);
+  $content = arabic_transliteration_replace("(^| )($non_transforming_prefixed_particles)?$alef([$standard_letters_without_lam])$sukun([$standard_letters_without_lam][$damma])", "\\1\\2$alef_with_wasla$damma\\3$sukun\\4", $content);
+  $content = arabic_transliteration_replace("(^| )($non_transforming_prefixed_particles)?$alef([$standard_letters])$sukun", "\\1\\2$alef_with_wasla\\3$sukun", $content);
+  // TODO: fi`l amr with lam
+  
+  // unmarked alef with wasla indicated by no sign on next letter, shadda on 2nd next letter
+  $content = arabic_transliteration_replace("$alef([$standard_letters][$standard_letters])$shadda", "$alef_with_wasla\\1$shadda", $content);
+
+  // regular alef, lam, and regular letter not marked by any tashkil
+  $content = arabic_transliteration_replace("(^| )($non_transforming_prefixed_particles)?$alef$lam([$standard_letters])", "\\1\\2$alef_with_wasla$lam-\\3", $content);
+
+  // alef-lam in "allah" should not have dash (=> al-lāh), so remove first lam for it not to be considered as a regular alef-lam
+	$content = arabic_transliteration_replace("$alef_with_wasla$lam$lam$shadda$fatha$dagger_alef?$ha", "$alef_with_wasla$lam$shadda$fatha$dagger_alef$ha", $content);
+
+  // alladhee/allatee should have alef with wasla
+	$content = arabic_transliteration_replace("(^| )($non_transforming_prefixed_particles)?$alef$lam$shadda$fatha?([$dhal$ta])$kasra?$ya", "\\1\\2$alef_with_wasla$lam$shadda$fatha\\3$kasra$ya", $content);
 
 
-	/* SPECIAL CASES */
+
+  /* DAGGER ALEF */
+
+  // rahman should have alef lam
+  $content = arabic_transliteration_replace("$lam$ra$shadda?$fatha?$hha$sukun?$mim$fatha?$nun", "$lam$ra$shadda$fatha$hha$sukun$mim$fatha$dagger_alef$nun", $content);
+	// lillah should have dagger alef
+  $content = arabic_transliteration_replace("(^| )($fa$fatha|$waw$fatha)?$lam$kasra?$lam(?:$shadda|$shadda$fatha)?$ha$kasra?($| )", "\\1\\2$lam$kasra$lam$shadda$fatha$dagger_alef$ha$kasra\\3", $content);
+	// dhalika should have dagger alef
+  $content = arabic_transliteration_replace("(^| )($non_transforming_prefixed_particles)?$dhal$fatha?$lam$kasra?$kaf$fatha?($| )", "\\1\\2$dhal$fatha$dagger_alef$lam$kasra$kaf$fatha\\3", $content);
 	
+	// dagger alef
+	$content = arabic_transliteration_replace($dagger_alef, $alef, $content);
+
+
+
+  /* SUN/MOON LETTERS */
+	
+	// sun letters
+	$content = arabic_transliteration_replace("(^| )$alef_with_wasla$lam([$sun_letters])$shadda", "\\1$alef_with_wasla\\2-\\2", $content);
+	
+	// moon letters
+	$content = arabic_transliteration_replace("(^| )$alef_with_wasla$lam$sukun?([$moon_letters])", "\\1$alef_with_wasla$lam-\\2", $content);
+
+  // 
+	
+  /*
   // prevent lam becoming "-" if succeeded by tanween
 	$content = arabic_transliteration_replace("لاً", "لْاً", $content);
 	// allah (common spelling: defective tashkil)
 	$content = arabic_transliteration_replace("الله", "ٱلْلَاه", $content);
 	$content = arabic_transliteration_replace("اللَّه", "ٱلْلَاه", $content);
-	// allah (remove -)
-	$content = arabic_transliteration_replace("$alef_with_wasla$lam$lam$shadda$fatha$dagger_alef", "$alef_with_wasla$lam$sukun$lam$fatha$dagger_alef", $content);
-	
-	// alladhee, alladheena, etc
-	$content = arabic_transliteration_replace("$alef_with_wasla$lam$lam$shadda$fatha", "$alef_with_wasla$lam$lam$fatha", $content);
 	
 	// ta marbutah without preceding fathah
 	$content = arabic_transliteration_replace("([^$fatha])$ta_marbuta", "\$1$fatha$ta_marbuta", $content);
-	
-	// unmarked alif-lam with sun letter
-	$content = arabic_transliteration_replace("(^| )$alef$lam([$sun_letters])", "\\1$alef_with_wasla$lam\\2", $content);
-	// unmarked alif-lam with moon letter
-	$content = arabic_transliteration_replace("(^| )$alef$lam([$moon_letters])", "\\1$alef_with_wasla$lam\\2", $content);
-	
-	// sun letters
-	$content = arabic_transliteration_replace("$alef_with_wasla$lam([^$lam])$shadda", "$alef_with_wasla\$1-\$1", $content);
-	$content = arabic_transliteration_replace("$alef$lam([^$lam])$shadda", "$alef_with_wasla\$1-\$1", $content);
-	$content = arabic_transliteration_replace("$lam([^$lam])$shadda", "$alef_with_wasla\$1-\$1", $content);
-	$content = arabic_transliteration_replace("$alef_with_wasla$lam$lam$shadda", "$alef_with_wasla$lam$lam", $content);
-	// moon letters
-	$content = arabic_transliteration_replace("([$alef_with_wasla$tashkil])$lam([^$tashkil$alef_maqsura])", "\$1$lam-\$2", $content);
-
-  // unmarked alef with wasla indicated by sukun on next letter
-  $content = arabic_transliteration_replace("$alef([$standard_letters])$sukun", "$alef_with_wasla\\1$sukun", $content);
 	
 	// ana
 	$content = arabic_transliteration_replace("$alef_with_sup_hamza$fatha?$nun$fatha?$alef$", "أَنَ$sukun", $content);
 	$content = arabic_transliteration_replace("$alef_with_sup_hamza$fatha?$nun$fatha?$alef ", "أَنَ$sukun ", $content);
 	// anti
 	$content = arabic_transliteration_replace("أَنْتِ$", "أَنْتِ$sukun", $content);
+  */
 	
 
 
 	/* SPECIAL LETTERS */
 
 	// tatwil
-	$content = arabic_transliteration_replace("ـ", "", $content);
-	// dagger alif
-	$content = arabic_transliteration_replace("$dagger_alef", "$alef", $content);
+	$content = arabic_transliteration_replace("ـ", "", $content); // todo: add to constants
+
 	// alif maqsura
 	$content = arabic_transliteration_replace("$alef_maqsura", "$alef", $content);
 
@@ -116,26 +131,28 @@ function arabic_transliteration_transform($content, $options){
 	$content = arabic_transliteration_replace("^$hamza", "'", $content);
 	$content = arabic_transliteration_replace(" $hamza", " '", $content);
 	// hamza inside words
-	$content = arabic_transliteration_replace("([^-])[$alef_with_sup_hamza$alef_with_sub_hamza$hamza$waw_with_hamza$ya_with_hamza]", "\$1-", $content);
+	$content = arabic_transliteration_replace("([^-])[$alef_with_sup_hamza$alef_with_sub_hamza$hamza$waw_with_hamza$ya_with_hamza]", "\$1'", $content);
 	$content = arabic_transliteration_replace("[$alef_with_sup_hamza$alef_with_sub_hamza$hamza$waw_with_hamza$ya_with_hamza]", "", $content);
 	
-	// alif with wasla preceded with haraka
-	$content = arabic_transliteration_replace("([$standard_harakat] )$alef_with_wasla", "\$1", $content);
+	// alif with wasla preceded by haraka
+	$content = arabic_transliteration_replace("([$standard_harakat])( )?$alef_with_wasla([$standard_harakat])?", "\\1\\2", $content);
 	
-	// alif with wasla preceded with long a
-	$content = arabic_transliteration_replace("$fatha$alef $alef_with_wasla", "$fatha ", $content);
-	// alif with wasla preceded with long u
-	$content = arabic_transliteration_replace("$damma$waw $alef_with_wasla", "$damma ", $content);
-	// alif with wasla preceded with long i
-	$content = arabic_transliteration_replace("$kasra$ya $alef_with_wasla", "$kasra ", $content);
+	// alif with wasla preceded by long a
+	$content = arabic_transliteration_replace("([$standard_letters])$fatha?$alef $alef_with_wasla([$standard_harakat])?", "\\1$fatha ", $content);
+	// alif with wasla preceded by long u
+	$content = arabic_transliteration_replace("$damma$waw $alef_with_wasla([$standard_harakat])?", "$damma ", $content);
+	// alif with wasla preceded by long i
+	$content = arabic_transliteration_replace("$kasra$ya $alef_with_wasla([$standard_harakat])?", "$kasra ", $content);
+	// alif with wasla preceded by sukun
+	$content = arabic_transliteration_replace("([$standard_letters])$sukun $alef_with_wasla([$standard_harakat])?", "\\1$kasra ", $content);
 	
 	// alif with wasla
-	$content = arabic_transliteration_replace("$alef_with_wasla", "a", $content);
+	//$content = arabic_transliteration_replace("$alef_with_wasla", "a", $content);
 	// alif with madda
 	$content = arabic_transliteration_replace("$alef_with_madda", "$alef", $content);
 
 	// ta marbuta at end of word sequence
-	$content = arabic_transliteration_replace("$ta_marbuta$", "$ha", $content);
+	$content = arabic_transliteration_replace("$ta_marbuta([$tashkil]*)$", "$ha\\1", $content);
 	
 	// question mark
 	$content = arabic_transliteration_replace("؟", "?", $content);
@@ -147,15 +164,33 @@ function arabic_transliteration_transform($content, $options){
 	// i - mi'ah
 	$content = arabic_transliteration_replace("$kasra$alef", "$kasra", $content);
 	
-	/* Shadda */
+	/* SHADDA */
+
 	// vowels
 	$content = arabic_transliteration_replace("$damma$waw$shadda", "$damma$waw$sukun$waw", $content);
 	$content = arabic_transliteration_replace("$kasra$ya$shadda", "$kasra$ya$sukun$ya", $content);
-	// regular
+	
+  // regular
 	$content = arabic_transliteration_replace("(.)$shadda", "\$1$sukun\$1", $content);
   
   //shadda of two-letter transliterated letters
   $content = arabic_transliteration_replace("($tha|$kha|$dhal|$shin|$ghayn)$sukun\\1", "\\1$sukun-\\1", $content);
+
+
+
+  /* STOP ON SUKUN */
+  
+	if($options['stop-on-sukun']){
+		// tanween
+		$content = arabic_transliteration_replace("$fathatan$alef$", "$fatha$alef", $content);
+		$content = arabic_transliteration_replace("$fathatan$", "", $content);
+		$content = arabic_transliteration_replace("$dammatan$", "", $content);
+		$content = arabic_transliteration_replace("$kasratan$", "", $content);
+		// harakat
+		$content = arabic_transliteration_replace("$fatha$", "", $content);
+		$content = arabic_transliteration_replace("$damma$", "", $content);
+		$content = arabic_transliteration_replace("$kasra$", "", $content);
+	}
 
   return $content;
 }
@@ -201,7 +236,7 @@ function arabic_transliteration_translate($content, $options){
     $nun => 'n',
     $ha => 'h',
 	
-    $hamza => '-',
+    $hamza => '\'',
     $ta_marbuta => 't',
 
     // waw
@@ -223,6 +258,11 @@ function arabic_transliteration_translate($content, $options){
     // vowels
     "$fatha$alef" => 'ā',
     $alef => 'ā',
+    
+    "$alef_with_wasla$fatha" => 'a',
+    "$alef_with_wasla$kasra" => 'i',
+    "$alef_with_wasla$damma" => 'u',
+    "$alef_with_wasla" => 'a',
 
     // harakat
     $fatha => 'a',
